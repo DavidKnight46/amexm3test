76c179ec5ed57d18c52bccb7ea6dffda509587de
David Knight <disco.david.85@gmail.com>
2024-08-18 21:27:02 +0800

    Docker-compose added. BuildScript modified.

--- buildScript.sh
+++ buildScript.sh
@@ -1,20 +1,10 @@
 #! /bin/bash
 
-git checkout dev
+git checkout docker
 git pull
+
+docker image build -t app .
 
 cd ./src/main/resources
 
-pwd
-
-#Run script for DB configuration
-psql -U postgres -f buildDB.sql
-
-cd ../../../
-
-pwd
-
-#Run SpringBoot application
-./gradlew build
-
-./gradlew bootRun
+docker-compose up
